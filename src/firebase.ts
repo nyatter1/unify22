cbbaa9e1b8b369c44aacf292a0ba0d5bf7fed3ad
com <haydensixseven@gmail.com>
2026-04-05 18:50:08 +0100

    Update Firebase configuration to include analytics
    Removed authentication and Firestore initialization, added analytics.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2ozwEYKEXO8o4RrZLB187rJ1PQjO9OTc",
@@ -14,5 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);

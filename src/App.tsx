9510e3bc1a3ab95435c3efbf335615c415b6bff1
com <haydensixseven@gmail.com>
2026-04-05 19:48:27 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from './supabase';
+import { db } from './firebase';
 import { UserProfile } from './types';
 import Auth from './components/Auth';
 import Onboarding from './components/Onboarding';
